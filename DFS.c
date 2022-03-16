c440056b07e452e23899e1fec0b25bad913b30d5
draitcs <100591542+draitcs@users.noreply.github.com>
2022-03-16 06:39:12 +0530

    Create DFS.c

---
+++ DFS.c
@@ -0,0 +1,43 @@
+void DFS(int [20][20],int,int[20],int);
+
+void main()
+{
+int n,a[20][20],i,j,visited[20],source;
+clrscr();
+printf("Enter the number of vertices\n");
+scanf("%d",&n);
+printf("Enter the adj matrix:\n");
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=n;j++)
+scanf("%d",&a[i][j]);
+}
+for(i=1;i<=n;i++)
+visited[i]=0;
+printf("Enter the source node\n");
+scanf("%d",&source);
+DFS(a,source,visited,n);
+for(i=1;i<=n;i++)
+{
+if(visited[i]==0)
+{
+printf("Graph is not connected\n");
+getch();
+exit(0);
+}
+}
+printf("Graph is connected\n");
+getch();
+}
+
+
+void DFS(int a[20][20],int u,int visited[20],int n)
+{
+int v;
+visited[u]=1;
+for(v=1;v<=n;v++)
+{
+if(a[u][v]==1&&visited[v]==0)
+DFS(a,v,visited,n);
+}
+}
